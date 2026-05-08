d8eebb0e1182b65265f70d335b8c1452d4ceaeaf
Pritam Bhudia <pritam1996@hotmail.co.uk>
2026-05-08 16:45:23 +0100

    updated script to handle classes that aren't a parent/child

--- generate_puml.py
+++ generate_puml.py
@@ -1,4 +1,4 @@
-from rdflib import Graph, RDFS, BNode
+from rdflib import Graph, RDF, OWL, RDFS, BNode
 from rdflib.namespace import split_uri
 
 def get_label(uri):
@@ -12,12 +12,25 @@ def get_label(uri):
 def graph_to_puml(g: Graph, filename: str = "diagram.puml"):
     lines = ["@startuml"]
 
+    # for classes that are a parent/child then add this relationship to the puml
+    classes_with_parent_or_child = set()
     for child, _, parent in g.triples((None, RDFS.subClassOf, None)):
         if isinstance(parent, BNode):
             continue  # skip parent blank nodes
+        classes_with_parent_or_child.add(child)
+        classes_with_parent_or_child.add(parent)
         child_name = get_label(child)
         parent_name = get_label(parent)
         lines.append(f'"{parent_name}" <|-- "{child_name}"')
+
+    # identify classes without a parent/child
+    classes_without_parent_or_child = set()
+    for cls in g.subjects((None, RDF.type, OWL.Class)):
+        if cls not in classes_with_parent_or_child:
+            classes_without_parent_or_child.add(cls)
+    
+    for x in classes_without_parent_or_child:
+        lines.append(f"class {get_label(x)}")
 
     lines.append("@enduml")
 
